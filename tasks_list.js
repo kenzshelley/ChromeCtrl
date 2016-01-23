e12e66367c1c9c3ac9ae35cbd7d602af8835e35b
kenzshelley <kenzshelley@gmail.com>
2016-01-23 12:31:58 -0500

    Added remaining tasks to list

--- tasks_list.js
+++ tasks_list.js
@@ -62,5 +62,33 @@ const tasks = {
         "little"
       ]
     }
+  },
+  "fastforward": {
+    "function": "fastForward",
+    "keywords": [
+      "fastforward",
+      "fast",
+      "forward",
+      "ahead"
+    ],
+    "scope": "content",
+  },
+  "reload": {
+    "function": "reload",
+    "keywords": [
+      "reload",
+      "refresh",
+      "load"
+    ],
+    "scope": "content"
+  },
+  "newTab": {
+    "function": "newTab",
+    "keywords": [
+      "new",
+      "tab", 
+      "create"
+    ],
+    "scope": "background"
   }
 }
