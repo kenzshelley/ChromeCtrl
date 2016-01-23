dcfa2d3440e82b4819744973f486ae77374cc6cc
jamie7132 <jfsmills@umich.edu>
2016-01-22 23:42:54 -0500

    init ios commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Penn Apps Winter 2016 Team:
+Nastasia Efremkina
+Addison Shelley
+Mackenzie Shelley
+Jamie Sookprasong
---
+++ ios/Remote/Remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Remote
+//
+//  Created by Jamie Sookprasong on 22/1/16.
+//  Copyright © 2016 Jamie Sookprasong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
