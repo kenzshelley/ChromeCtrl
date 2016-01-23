8bbd2d78a1994ac11f4492bb77abd9235f08551e
jamie7132 <jfsmills@umich.edu>
2016-01-23 04:31:20 -0500

    objc working. next step firebase

--- ios/Remote/Remote/ViewController.m
+++ ios/Remote/Remote/ViewController.m
@@ -6,9 +6,16 @@
 //  Copyright © 2016 Jamie Sookprasong. All rights reserved.
 //
 
+// adapted from SoundHound's Houndify Demo
+
 #import "ViewController.h"
+#import <HoundSDK/HoundSDK.h>
+
+#define VOICE_SEARCH_END_POINT       @"https://api.houndify.com/v1/audio"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UILabel *textFromSpeech;
 
 @end
 
@@ -16,12 +23,82 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // Setup UI
+    
+    self.textFromSpeech.text = nil;
+    
+    UIImage* image = [UIImage imageNamed:@"ic-hound-small"];
+    
+    UIImageView* imageView = [[UIImageView alloc] initWithImage:image];
+    
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    imageView.frame = CGRectMake(0, 0, 32, 32);
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]
+                                             initWithCustomView:imageView];
+}
+- (IBAction)listen:(UIButton *)sender {
+    self.textFromSpeech.text = nil;
+    
+    NSURL* URL = [NSURL URLWithString:VOICE_SEARCH_END_POINT];
+    
+    NSDictionary* requestInfo = @{
+                                  
+                                  // Insert request parameters
+                                  };
+    
+    // Show listening screen
+    
+    [Houndify.instance
+     presentListeningViewControllerInViewController:self.navigationController
+     fromView:sender
+     requestInfo:requestInfo
+     endPointURL:URL
+     responseHandler:^(NSError* error, HoundDataHoundServer* response, NSDictionary* dictionary) {
+         
+         if (error)
+         {
+             // Check for errors
+             
+             if ([error.domain isEqualToString:HoundVoiceSearchErrorDomain]
+                 && error.code == HoundVoiceSearchErrorCodeCancelled)
+             {
+                 self.textFromSpeech.text = @"Search cancelled";
+             }
+             else if ([error.domain isEqualToString:HoundVoiceSearchErrorDomain]
+                      && error.code == HoundVoiceSearchErrorCodeAuthenticationFailed)
+             {
+                 self.textFromSpeech.text = @"Authentication failed";
+             }
+             else
+             {
+                 self.textFromSpeech.text = @"Search failed";
+             }
+         }
+         else
+         {
+             // Display written response in UI
+             
+             HoundDataCommandResult* commandResult = response.allResults.firstObject;
+             
+             self.textFromSpeech.text = commandResult.writtenResponse;
+             
+             // Any properties from the documentation can be accessed through the keyed accessors, e.g.:
+             
+             NSDictionary* nativeData = commandResult[@"NativeData"];
+             
+             NSLog(@"NativeData: %@", nativeData);
+         }
+         
+         [self dismissSearch];
+     }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)dismissSearch
+{
+    [Houndify.instance dismissListeningViewControllerAnimated:YES completionHandler:^{}];
 }
 
 @end
