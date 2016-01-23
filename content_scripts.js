ae8612f5db41ba73d1360e7fbc638ea1938f8c8b
Nastasia <aefrem@seas.upenn.edu>
2016-01-23 00:06:06 -0500

    wrote functions for pause/play videos

---
+++ content_scripts.js
@@ -0,0 +1,19 @@
+function pausePlay(isPlay) {
+	var videoTagElements = document.getElementsByTagName("video");
+	if (videoTagElements.length != 0) {
+		if (isPlay == true) {
+			videoTagElements[0].play();
+		} else {
+			videoTagElements[0].pause();
+		}		
+	} else if (typeof ifp !== undefined) {
+		if (isPlay == true) {	
+			ifp.play();
+		} else {
+			ifp.pause();
+		}
+	} else {
+		console.log("wrong move, try again");
+	}
+}
+	
