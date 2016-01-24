953deb85ab38eec0558713e68e5e7fa2c0fac8c6
kenzshelley <kenzshelley@gmail.com>
2016-01-24 03:51:24 -0500

    actually add page list

---
+++ page_list.js
@@ -0,0 +1,7 @@
+const pages = {
+  "google": "https://www.google.com",
+  "facebook": "https://www.facebook.com",
+  "twitter": "https://www.twitter.com",
+  "netflix": "https://www.netflix.com",
+  "youtube": "https://www.youtube.com"
+}
