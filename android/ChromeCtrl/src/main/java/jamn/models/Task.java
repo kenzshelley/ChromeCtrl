f9c82fb312b7dbd22eab220665c52a87d6fe88db
kenzshelley <kenzshelley@gmail.com>
2016-02-06 16:35:37 -0500

    Completed task feedback working in basic form

---
+++ android/ChromeCtrl/src/main/java/jamn/models/Task.java
@@ -0,0 +1,16 @@
+package jamn.models;
+
+public class Task {
+    private String name;
+
+    public void setName(String taskName) {
+        name = taskName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Task() {
+    }
+}
