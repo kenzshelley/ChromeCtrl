6ed4a76adf79ec639573400196c82b6b4c2e2dbd
kenzshelley <kenzshelley@gmail.com>
2016-02-10 22:51:50 -0500

    Make speech recognition appear in a custom dialog

---
+++ android/ChromeCtrl/src/main/java/jamn/fragments/SpeechDialogFragment.java
@@ -0,0 +1,77 @@
+package jamn.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.List;
+
+import jamn.R;
+
+public class SpeechDialogFragment extends LinearLayout {
+    private SpeechRecognizer mSpeechRecognizer;
+    private Intent mSpeechRecognizerIntent;
+    private SpeechRecognitionFragment.SpeechCallback mSpeechCallback;
+
+    public SpeechDialogFragment(final Context context, SpeechRecognitionFragment.SpeechCallback speechCallback) {
+       super(context);
+
+        mSpeechCallback = speechCallback;
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(context);
+        mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+        SpeechRecognitionListener listener = new SpeechRecognitionListener();
+        mSpeechRecognizer.setRecognitionListener(listener);
+
+        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.fragment_speech_dialog, null);
+
+        addView(v);
+    }
+
+    protected class SpeechRecognitionListener implements RecognitionListener {
+        @Override
+        public void onBeginningOfSpeech() {}
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {}
+
+        @Override
+        public void onEndOfSpeech() {
+            mSpeechCallback.handleMessage(null);
+            Log.d("speech", "End of speech");
+        }
+
+        @Override
+        public void onError(int error) {}
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            Log.d("Partial results", partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION).toString());
+        }
+
+        @Override
+        public void onReadyForSpeech(Bundle params) {}
+
+        @Override
+        public void onResults(Bundle results) {
+            List<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            Log.d("results", matches.toString());
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {}
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {}
+    }
+}
---
+++ android/ChromeCtrl/src/main/java/jamn/fragments/TaskCard.java
@@ -0,0 +1,8 @@
+package jamn.fragments;
+
+import android.support.v7.widget.CardView;
+
+import jamn.models.Task;
+
+public class TaskCard {
+}
